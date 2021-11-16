52ee98add21d2dac24ff8254b138fa604e476ac3
lucas.sousa <lucas.sousa@codificar.com.br>
2021-11-16 02:41:21 -0300

    routes static

--- api/index.mjs
+++ api/index.mjs
@@ -8,9 +8,24 @@ import './services/jobs.mjs'
 
 const app = express()
 
-app.use(express.static(path.resolve() + '/dist/'))
-app.use('/uploads', express.static(path.resolve() + '/uploads'))
+// app.get('*', (req, res) => {
+//     res.json({
+//         msg: 'test req'
+//     })
+// })
+
 app.use('/api/issues', routesIssues)
+
+// app.use('*', express.static(path.resolve() + '/dist/'))
+
+// app.use('/uploads', express.static(path.resolve() + '/uploads'))
+app.use('/', express.static(path.resolve() + '/dist/'))
+app.use('/importacao', express.static(path.resolve() + '/dist/'))
+app.use('/issues', express.static(path.resolve() + '/dist/'))
+
+// app.use('/dist', express.static(path.resolve() + '/dist/'))
+
+// console.log(path.resolve() + '/dist/')
 
 const port = '8000'
 app.listen(port, () => { console.log('http://localhost:' + port) })
--- src/services/api.js
+++ src/services/api.js
@@ -6,6 +6,10 @@ const baseUrl = () => {
         return 'http://dev.appmarketplace.com.br:8000'
     }
 
+    // if (host === '127.0.0.1:8081') {
+    //     return 'http://127.0.0.1:8081'
+    // }
+
     return 'http://localhost:8000'
 
 }
