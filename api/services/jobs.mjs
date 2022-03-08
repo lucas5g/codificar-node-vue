7447582624c457200e6594b330a7a34eeca13076
Italo Costa <italo.costa@codificar.com.br>
2022-03-08 18:21:28 -0300

    first commit

--- api/services/jobs.mjs
+++ api/services/jobs.mjs
@@ -1,8 +1,10 @@
 // import axios from 'axios';
+import axios from 'axios';
 import cron from 'node-cron'
 import { AutoController } from '../controllers/AutoController.mjs';
 import { BotController } from "../controllers/BotController.mjs";
 import { IssueController } from '../controllers/IssueController.mjs';
+import { commandFunction } from '../helpers/index.mjs';
 
 cron.schedule('0 55 23 * * 1-5', () => {
 
@@ -21,9 +23,18 @@ cron.schedule('0 26 16 * * 1-5', () => {
 
 
 (async() => {
-    // BotController.reportDaily()
-    // BotController.reportCompleted()
-    // BotController.testBot()
+    // const url = `https://${process.env.REDMINE_KEY}@redmine.codificar.com.br/issues.json?project_id=clickfood&status_id=closed&limit=60`
+    // const { data } = await axios.get(url)
+
+    // commandFunction('rm  redmine_pdf/*.pdf')
+
+    // data.issues.forEach( issue => {
+    //     const urlPdf = `curl https://${process.env.REDMINE_KEY}@redmine.codificar.com.br/issues/${issue.id}.pdf --output redmine_pdf/${issue.id}.pdf`
+    //     console.log(urlPdf)
+    //     commandFunction(urlPdf)
+    // })
+    //esse comando nao funciona
+    // commandFunction('zip -r redmine.zip redmine_pdf')
 
 
 })()
