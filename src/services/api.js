549e197b52f71cc370f6e0b38a819e7d83556819
lucas.sousa <lucas.sousa@codificar.com.br>
2022-03-14 13:33:25 -0300

    -m

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,8 @@
 import axios from 'axios'
 
-const baseUrl = () => {
-    const { host } = window.location
-    if (host === 'demo.appmarketplace.com.br:8000' || host === 'version.aplicativoderestaurante.com.br') {
-        // return 'http://demo.appmarketplace.com.br:8000/api/'
-        return 'http://version.aplicativoderestaurante.com.br/api'
-    }
-
-    // if (host === '127.0.0.1:8081') {
-    //     return 'http://127.0.0.1:8081'
-    // }
-
-    return 'http://localhost:8000/api'
-
-}
-
 export const api = axios.create({
-    baseURL: `${baseUrl()}/`
+    baseURL: process.env.VUE_APP_API
 })
 
-console.log('BASE_URL' + baseUrl())
 console.log('0314')
+console.log(process.env.VUE_APP_API)
